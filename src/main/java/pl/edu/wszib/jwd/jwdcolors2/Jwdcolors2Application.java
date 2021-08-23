009762142775d06f4857b93256af7cd397c1cd29
KrisD1977 <derela@student.wszib.edu.pl>
2021-08-23 12:58:50 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/jwd/jwdcolors2/Jwdcolors2Application.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwd.jwdcolors2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwdcolors2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jwdcolors2Application.class, args);
+    }
+
+}
